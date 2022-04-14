9a050a85091863f80f5b274e1aec199993130520
KamauRuth <rkamau573@gmail.com>
2022-04-14 11:55:25 +0300

    My Commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of a number
+ * @n: number to be computed
+ * Return: 0
+*/
+
+int _abs(int n)
+{	int r;
+
+	r = abs(n);
+	return (r);
+}
